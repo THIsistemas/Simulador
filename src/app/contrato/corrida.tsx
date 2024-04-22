954a488db7f757f1973ddd2bf76e82082d2a53b0
Luis <amador.c.luis@hotmail.com>
2024-04-22 13:51:48 -0600

    simulador update

--- src/app/contrato/corrida.tsx
+++ src/app/contrato/corrida.tsx
@@ -3,16 +3,16 @@ import React from 'react'
 import Imprimir from './imprimir'
 
 const Corrida = ({columns, corrida}:any) => {
-
+	
   return (
 	<>	
 
 	<Table aria-label="Example table with dynamic content" className="min-w-24 p-10">
       <TableHeader columns={columns}>
-        {(column) => <TableColumn className="text-red-700 font-bold" key={column.key}>{column.label}</TableColumn>}
+        {(column:any) => <TableColumn className="text-red-700 font-bold" key={column.key}>{column.label}</TableColumn>}
       </TableHeader>
       <TableBody items={corrida}>
-        {(item) => (
+        {(item:any) => (
           <TableRow key={item.numero}>
             {(columnKey) => <TableCell className=" font-bold ">{getKeyValue(item, columnKey)}</TableCell>}
           </TableRow>
